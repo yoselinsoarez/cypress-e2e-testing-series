97da6cb3de8a9f3e27f5ccccdd318e6adcf511b1
yoselinsoarez <yoselinsoarez93@gmail.com>
2023-02-20 17:13:24 -0300

    testing multiple pages with cypress

---
+++ e2e/MyFirstTest.cy.js
@@ -0,0 +1,15 @@
+describe("My First Test", () => {
+  it("verify title-positive", () => {
+    //steps
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+
+    cy.title().should("eq", "OrangeHRM");
+  });
+
+  it("verify title-negative", () => {
+    //steps
+    cy.visit("https://opensource-demo.orangehrmlive.com/");
+
+    cy.title().should("eq", "OrangeHRM555");
+  });
+});
