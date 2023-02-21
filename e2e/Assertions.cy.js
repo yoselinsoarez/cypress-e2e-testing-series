eaa226f2769646598ea34d088d5275ae3238511e
yoselinsoarez <yoselinsoarez93@gmail.com>
2023-02-21 13:47:23 -0300

    fixing code

--- e2e/Assertions.cy.js
+++ e2e/Assertions.cy.js
@@ -43,14 +43,14 @@ describe("Assertions demo", () => {
 
     cy.get("input[placeholder='Username']").type("Admin"); //Provide a value into input box
     cy.get("input[placeholder='Username']").should("have.value", "Admin");
+  });
 
-    it("Explicit assertions", () => {
-      cy.visit(
-        "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
-      );
-      cy.get("input[placeholder='Username']").type("Admin");
-      cy.get("input[placeholder='Password']").type("admin123");
-      cy.get("button[type='submit']").click();
-    });
+  it("Explicit assertions", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.get("input[placeholder='Username']").type("Admin");
+    cy.get("input[placeholder='Password']").type("admin123");
+    cy.get("button[type='submit']").click();
   });
 });
