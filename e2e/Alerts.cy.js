8b8c1a4cfd1c6bd2eddcd542c381b7509d8931bc
yoselinsoarez <yoselinsoarez93@gmail.com>
2023-02-22 10:38:36 -0300

    testing confirm alert

--- e2e/Alerts.cy.js
+++ e2e/Alerts.cy.js
@@ -11,4 +11,16 @@ describe("Alerts", () => {
     //alert window automatically closed by cypress
     cy.get("#result").should("have.text", "You successfully clicked an alert");
   });
+
+  /*	2. JavaScript confirm alert: It will have some text with ok and cancel buttons
+  it.only  run jus this chunk of code */
+  it.only("JS confirm alert ok button", () => {
+    cy.visit("https://the-internet.herokuapp.com/javascript_alerts");
+    cy.get("button[onclick='jsConfirm()']").click();
+    cy.on("window:confirm", (t) => {
+      expect(t).to.contains("I am a JS Confirm");
+    });
+    //cypress automatically closed alert window by using ok button-default
+    cy.get("#result").should("have.text", "You clicked: Ok");
+  });
 });
