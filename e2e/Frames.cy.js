4079f95323aba2a9f017ad8eca8b688c9c11832c
yoselinsoarez <yoselinsoarez93@gmail.com>
2023-02-23 10:51:40 -0300

    testing frames approaches

--- e2e/Frames.cy.js
+++ e2e/Frames.cy.js
@@ -1,14 +1,31 @@
+import 'cypress-iframe';
+
 describe('handling frames', () => {
     it('approach1', () => {
         cy.visit('https://the-internet.herokuapp.com/iframe');
 
         const iframe = cy
-            .get('#mce_0_ifr')
+            .get('#mce_0_ifr') //get the frame by id
             .its('0.contentDocument.body')
             .should('be.visible')
             .then(cy.wrap);
 
         iframe.clear().type('Learning cypress {ctrl+a}');
+        cy.get("[aria-label='Bold']").click();
+    });
+
+    it('approach2 by using custom command', () => {
+        cy.visit('https://the-internet.herokuapp.com/iframe');
+
+        cy.getIframe('#mce_0_ifr').clear().type('Learning cypress {ctrl+a}');
+
+        cy.get("[aria-label='Bold']").click();
+    });
+
+    it.only('approach2 by using cypress plugin', () => {
+        cy.visit('https://the-internet.herokuapp.com/iframe');
+        cy.frameLoaded('#mce_0_ifr'); //Load the frame
+        cy.iframe('#mce_0_ifr').clear().type('Learning cypress {ctrl+a}');
         cy.get("[aria-label='Bold']").click();
     });
 });
--- support/commands.js
+++ support/commands.js
@@ -25,3 +25,12 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 /// <reference types="Cypress" />
+
+//custom command
+Cypress.Commands.add('getIframe', (iframe) => {
+    return cy
+        .get(iframe)
+        .its('0.contentDocument.body')
+        .should('be.visible')
+        .then(cy.wrap);
+});
